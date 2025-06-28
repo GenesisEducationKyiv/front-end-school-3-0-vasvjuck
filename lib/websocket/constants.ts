b9a32bb7f6b5212ca983f27c7fc2b725d297d94c
Vasyl Vasiuk <vasiuk.br09@gmail.com>
2025-06-28 23:01:00 +0200

    chore: move to ts file

---
+++ lib/websocket/constants.ts
@@ -0,0 +1,19 @@
+const SAMPLE_TRACKS: readonly string[] = [
+    "Bohemian Rhapsody - Queen",
+    "Hotel California - Eagles",
+    "Stairway to Heaven - Led Zeppelin",
+    "Imagine - John Lennon",
+    "Hey Jude - The Beatles",
+    "Smells Like Teen Spirit - Nirvana",
+    "Like a Rolling Stone - Bob Dylan",
+    "Yesterday - The Beatles",
+    "Good Vibrations - The Beach Boys",
+    "Johnny B. Goode - Chuck Berry",
+    "What's Going On - Marvin Gaye",
+    "My Generation - The Who",
+    "A Change Is Gonna Come - Sam Cooke",
+    "Respect - Aretha Franklin",
+    "I Want to Hold Your Hand - The Beatles"
+] as const;
+
+export { SAMPLE_TRACKS }; 
